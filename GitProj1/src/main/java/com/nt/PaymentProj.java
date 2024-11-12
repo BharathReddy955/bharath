6d1220cd51b9c6ad619442a4641df11c49bd55c8
Bharath Reddy <bsrassignment@gmail.com>
2024-11-12 08:57:25 +0530

    DEV payment

--- GitProj1/src/main/java/com/nt/PaymentProj.java
+++ GitProj1/src/main/java/com/nt/PaymentProj.java
@@ -1,8 +1,22 @@
 package com.nt;
 
 public class PaymentProj {
+	public static void main(String[] args) {
+	System.out.println(	ram());
+	}
 
 	public String addAmount(int x){
 		return x+"is credited in account";
 	}	
+
+public static String ram() {
+	return  " HARE RAMA HARE RAMA RAMA RAMA HARE HARE  "
+			+ " HARE KRISHNA HARE KRISHNA KRISHNA KRISHNA HARE HARE " ;
 }
+	
+	
+	
+}
+
+
+
